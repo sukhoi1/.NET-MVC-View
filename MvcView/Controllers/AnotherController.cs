e957363eff96ae1453017022119282626454430c
Anton Lyhin <Anton_Lyhin@epam.com>
2016-12-21 20:04:10 +0300

    v0.003 - section, child view attriute, view as partial helper, view as action helper;

---
+++ MvcView/Controllers/AnotherController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace MvcView.Controllers
+{
+    public class AnotherController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MvcView/Controllers/HomeController.cs
+++ MvcView/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace MvcView.Controllers
@@ -22,6 +23,37 @@ namespace MvcView.Controllers
         }
 
         public ActionResult Layout()
+        {
+            return View();
+        }
+
+        public ActionResult Section()
+        {
+            return View();
+        }
+
+        public ActionResult SectionOptional()
+        {
+            return View();
+        }
+
+        public ActionResult Partial()
+        {
+            return View();
+        }
+
+        public ActionResult ViewNoModel()
+        {
+            return View(new DateTime().Date);
+        }
+
+        public ActionResult ChildAction()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult ChildActionAttribute()
         {
             return View();
         }
